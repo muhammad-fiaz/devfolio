b1d2145dcf42ab1b19573b01d2d20fde93b33198
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-07-28 20:18:40 +0530

    Initial Commit -> Added Next.js Support

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from "next/link";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        Coming Soon <Link href="/docs">Docs</Link>
+    </div>
+  );
+};
+
+export default Home;
